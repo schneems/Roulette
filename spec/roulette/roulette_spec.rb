04dce16dece4fe579ada09c7b3997eda5472e386
Schneems <richard.schneeman@gmail.com>
2011-07-11 11:24:21 -0500

    make sure we're not operating on an array of an array

--- spec/roulette/roulette_spec.rb
+++ spec/roulette/roulette_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 class FakeKeyValueStore
-  
+
 end
 
 
@@ -10,25 +10,37 @@ describe Roulette do
         @kv_store = FakeKeyValueStore.new
         @roulette = Roulette.new(@kv_store)
       end
-      
+
       it 'passes, key and value to the stored KV_store' do
         @kv_store.should_receive(:sadd).with("foo", "bar")
         @roulette.sadd("foo", "bar")
       end
-      
+
       it 'passes, key and value to the stored KV_store' do
         @kv_store.should_receive(:sadd).with("foo", "bar")
         @roulette.sadd("foo", "bar")
       end
-      
+
       it 'passes key to the stored KV_store' do
         @kv_store.should_receive(:scard).with("foo")
         @roulette.scard("foo")
       end
-      
+
       it 'passes arbitrary number of elements to the stored KV_store' do
         @kv_store.should_receive(:fake_method).with(1,2,3,4,5,6,7,8,9,0)
         @roulette.fake_method(1,2,3,4,5,6,7,8,9,0)
+      end
+    end
+
+    describe 'stores' do
+      before do
+        @kv_store = FakeKeyValueStore.new
+      end
+
+      it 'passes multiple stores into a single array (doesn make an array of an array)' do
+        store_array = [@kv_store, @kv_store, @kv_store]
+        @roulette = Roulette.new(store_array)
+        @roulette.stores.should eq(store_array)
       end
     end
 end
